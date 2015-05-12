19736d8ad47f7eca92619b5c89311d6e53ae218d
Nadav <Nadav2051@gmail.com>
2015-05-12 09:40:12 +0300

    GREEN: Added test for non existant name

--- CrowdMap.py
+++ CrowdMap.py
@@ -1,3 +1,6 @@
 class CrowdMap():
-    def getAllPostsFor(self, findName):
-        return ["Or"]
+    def __init__(self, initPostList):
+        self.postList = initPostList
+        
+    def getAllPostsFor(self, findName):            
+        return [post for post in self.postList if findName in post]
--- FindAPersonTests.py
+++ FindAPersonTests.py
@@ -3,15 +3,18 @@ from CrowdMap import CrowdMap
 
 class FindAPersonTests(unittest.TestCase):
     def setUp(self):
-        self.crowdmap = CrowdMap()
+        postList = ["Or", 
+                    "Or A.",
+                    "Misisng Cowboy"]
+        self.crowdmap = CrowdMap(postList)
         
     def test_getAllPostsForName(self):
         posts = self.crowdmap.getAllPostsFor("Or")
-        self.assertIn("Or", posts)
-        
-    def test_getAllPostsForMissingName(self):
-        posts=self.crowdmap.getAllPostsFor("Or2")
-        self.assertNotIn("Or2",posts)
+        self.assertEquals( ["Or", "Or A."], posts)
+    
+    def test_getAllPostsForNonExistantName(self):
+        posts = self.crowdmap.getAllPostsFor("Joe")
+        self.assertEquals([], posts)
 
 if __name__ == '__main__':
     unittest.main()

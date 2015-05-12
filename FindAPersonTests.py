855418977ad5268929914f2345007c3aee8ea6ea
Nadav <Nadav2051@gmail.com>
2015-05-12 09:16:19 +0300

    Added another unit test

--- FindAPersonTests.py
+++ FindAPersonTests.py
@@ -8,7 +8,10 @@ class FindAPersonTests(unittest.TestCase):
     def test_getAllPostsForName(self):
         posts = self.crowdmap.getAllPostsFor("Or")
         self.assertIn("Or", posts)
-
+        
+    def test_getAllPostsForMissingName(self):
+        posts=self.crowdmap.getAllPostsFor("Or2")
+        self.assertNotIn("Or2",posts)
 
 if __name__ == '__main__':
     unittest.main()

41245f1130a008e1dbe254c833e59a04455ff3e9
Nadav <Nadav2051@gmail.com>
2015-05-12 09:51:00 +0300

    RED: Added failing test for a name that has no location informatoin

--- FindAPersonTests.py
+++ FindAPersonTests.py
@@ -20,7 +20,10 @@ class FindAPersonTests(unittest.TestCase):
     def test_existingLocatinInformationReturnsTrue(self):
         locationExist = self.crowdmap.isLocationForName("Or")
         self.assertTrue(locationExist)
-
+        
+    def test_nonExistingLocatinInformationReturnsTrue(self):
+        locationExist = self.crowdmap.isLocationForName("Lassy")
+        self.assertFalse(locationExist)
 
 if __name__ == '__main__':
     unittest.main()

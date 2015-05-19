9fe135e46113fadfc4ae96120e6b0ef567b3e900
Nadav <Nadav2051@gmail.com>
2015-05-19 09:26:31 +0300

    RED: isLocationForName now uses empty location list

--- CrowdMap.py
+++ CrowdMap.py
@@ -1,3 +1,4 @@
+from LocationService import LocationService
 class CrowdMap():
     def __init__(self, initPostList):
         self.postList = initPostList
@@ -6,4 +7,12 @@ class CrowdMap():
         return [post for post in self.postList if name in post]
     
     def isLocationForName(self, name):
+        locations = []
+        posts = self.getAllPostsFor(name)
+        for post in posts:
+            for location in locations:
+                if location in post:
+                    return True
+        return False
+            
         
--- LocationService.py
+++ LocationService.py
@@ -1,5 +1,5 @@
 class LocationService():
     def __init__(self):
-        self.locations = ['Bangkok', 'Israel', 'Langtang']
+        self.locations = ['Bangkok', 'Israel', 'Langtang valley']
     def getLocations(self):
         return self.locations
